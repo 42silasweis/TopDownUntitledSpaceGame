7612c45fa159f2673a26402ae25f411402c6b0af
Silas Weis <18.silas.weis@ksd.org>
2019-10-28 13:55:32 -0700

    Made room for backgrounds

--- TopDownUntitledSpaceGame/Assets/Scripts/SpawnScript.cs
+++ TopDownUntitledSpaceGame/Assets/Scripts/SpawnScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Variable to store the enemy prefab
+    public GameObject enemy;
+
+// Variable to know how fast we should create new enemies
+    public float spawnTime = 2;
+
     void Start()
     {
-        
+        // Call the 'addEnemy' function in 0 second
+        // Then every 'spawnTime' seconds
+        InvokeRepeating("addEnemy", 0, spawnTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    // New function to spawn an enemy
+    void addEnemy()
     {
-        
+        // Get the renderer component of the spawn object
+        rd = GetComponent<SpriteRenderer>();
+
+        // Position of the left edge of the spawn object
+        // It's: (position of the center) minus (half the width)
+        x1 = transform.position.x - rd.bounds.size.x / 2;
+
+        // Same for the right edge
+        x2 = transform.position.x + rd.bounds.size.x / 2;
+
+        // Randomly pick a point within the spawn object
+        spawnPoint = Vector2(Random.Range(x1, x2), transform.position.y);
+
+        // Create an enemy at the 'spawnPoint' position
+        Instantiate(enemy, spawnPoint, Quaternion.identity);
     }
 }
