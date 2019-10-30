742ac5cdf0d883ecdef9bf5cbb6bc28e3123abbd
42silasweis <silasweis18@gmail.com>
2019-10-29 18:46:28 -0700

    Point objects now get destroy when the play dies

--- TopDownUntitledSpaceGame/Assets/Scripts/MassKill.cs
+++ TopDownUntitledSpaceGame/Assets/Scripts/MassKill.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class MassKill : MonoBehaviour
 {
+    public bool killAll = true;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "MassKillEnemies")
+        if (collision.gameObject.tag == "MassKillEnemies" && killAll == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTrigger2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MassKillEnemies" && killAll == true)
         {
             Destroy(gameObject);
         }
