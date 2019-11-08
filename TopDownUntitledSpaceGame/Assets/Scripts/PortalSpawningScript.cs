2cbf5e45476b8664bafd03f23819caa0165e9b2c
Silas Weis <18.silas.weis@ksd.org>
2019-11-08 13:16:29 -0800

    Portal works as intended with UI that tells you when it activates while standing on it

--- TopDownUntitledSpaceGame/Assets/Scripts/PortalSpawningScript.cs
+++ TopDownUntitledSpaceGame/Assets/Scripts/PortalSpawningScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortalSpawningScript : MonoBehaviour
 {
@@ -12,15 +13,29 @@ public class PortalSpawningScript : MonoBehaviour
     public float portalTimer;
     public float timeBeforeTP = 2.0f;
     public float portalDelay;
-    bool portalOn;
-    bool portalOn2;
+    bool portalOn = false;
+    bool portalOn2 = false;
     public GameObject portalParticles;
     GameObject boss;
     private SpriteRenderer spriteRenderer;
     public Sprite[] portalSprite;
+
+
+    bool sliderSpriteOn = false;
+    Slider sliderBar;
+    GameObject sliderBar1;
+    GameObject sliderBar2;
     // Start is called before the first frame update
     void Start()
     {
+        sliderBar = GameObject.FindGameObjectWithTag("PortalTimerSlider").GetComponent<Slider>();
+        sliderBar1 = GameObject.FindGameObjectWithTag("PortalSliderBackground");
+        sliderBar2 = GameObject.FindGameObjectWithTag("PortalSliderFill");
+        sliderBar1.GetComponent<Image>().enabled = false;
+        sliderBar2.GetComponent<Image>().enabled = false;
+        sliderBar.maxValue = timeBeforeTP;
+        sliderBar.value = portalTimer;
+
         GetComponent<PolygonCollider2D>().enabled = false;
         GetComponent<Animator>().enabled = false;
         GetComponent<SpriteRenderer>().enabled = false;
@@ -49,27 +64,46 @@ public class PortalSpawningScript : MonoBehaviour
             GetComponent<PolygonCollider2D>().enabled = true;
             GetComponent<SpriteRenderer>().enabled = true;
             GetComponent<Animator>().enabled = true;
-            spriteRenderer.sprite = portalSprite[0];
+            spriteRenderer.sprite = portalSprite[0];            
         }
-            if(PointCount > neededPoints && timer > portalDelay && portalOn2 == false)
+            if(portalOn == true && timer > portalDelay && portalOn2 == false)
             {
                 portalOn2 = true;
                 Instantiate(portalParticles, transform.position, transform.rotation);
                 spriteRenderer.sprite = portalSprite[6];
                 GetComponent<SpriteRenderer>().enabled = true;
                 GetComponent<Animator>().enabled = false;
-            }
+            Debug.Log(portalOn2);
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        portalTimer += Time.deltaTime;
-        if(collision.gameObject.tag == "Player" && portalTimer > timeBeforeTP)
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            portalTimer += Time.deltaTime;
+            sliderBar.value = portalTimer;
+            if (sliderSpriteOn == false)
+            {
+                sliderSpriteOn = true;
+                sliderBar1.GetComponent<Image>().enabled = true;
+                sliderBar2.GetComponent<Image>().enabled = true;
+            }
+        }
+        if (collision.gameObject.tag == "Player" && portalTimer > timeBeforeTP)
         {
             SceneManager.LoadScene("LevelSelect");
         }
     }
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        portalTimer = 0;
+        
+        if (other.gameObject.tag == "Player")
+        {
+            portalTimer = 0;
+            sliderBar1.GetComponent<Image>().enabled = false;
+            sliderBar2.GetComponent<Image>().enabled = false;
+            sliderSpriteOn = false;
+        }
     }
 }
